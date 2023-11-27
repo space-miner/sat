071dd5bd4ee3873c98046bc78ed1787cdf052963
tony <tchow@tutamail.com>
2023-11-26 21:03:30 -0500

    change main to use sat_lib and take in input from stdin

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,6 @@
 open Base
 open Stdio
-open Assignment
-open Clause
-open Cnf_formula
-open Literal
-open Util
+open Sat_lib
 
 module Solver_Result = struct
   type t =
@@ -24,7 +20,7 @@ end
 module type Solver = sig
   type t = Solver_Result.t
 
-  val solve : CNF_Formula.t -> Assignment.t -> t
+  val solve : Cnf_formula.t -> Assignment.t -> t
 end
 
 module DPLL : Solver = struct
@@ -32,16 +28,16 @@ module DPLL : Solver = struct
 
   let rec solve fml assignment =
     With_return.with_return (fun { return } ->
-      let fml, assignment = CNF_Formula.pure_literal_elimination fml assignment in
-      let fml, assignment = CNF_Formula.unit_propogation fml assignment in
-      let var_opt = CNF_Formula.choose_variable_opt fml assignment in
+      let fml, assignment = Cnf_formula.pure_literal_elimination fml assignment in
+      let fml, assignment = Cnf_formula.unit_propogation fml assignment in
+      let var_opt = Cnf_formula.choose_variable_opt fml assignment in
       match var_opt with
       | Some var ->
         (* this feels jank here, assigning and making calls to eliminate *)
         let true_assignment = Assignment.add assignment var true in
         let false_assignment = Assignment.add assignment var false in
-        let true_fml = CNF_Formula.eliminate fml [ var ] in
-        let false_fml = CNF_Formula.eliminate fml [ Literal.neg var ] in
+        let true_fml = Cnf_formula.eliminate fml [ var ] in
+        let false_fml = Cnf_formula.eliminate fml [ Literal.neg var ] in
         let true_result = solve true_fml true_assignment in
         (match true_result with
          | Solver_Result.Satisfiable _ -> return true_result
@@ -49,16 +45,16 @@ module DPLL : Solver = struct
            let false_result = solve false_fml false_assignment in
            false_result)
       | None ->
-        if CNF_Formula.is_satisfiable fml
+        if Cnf_formula.is_satisfiable fml
         then Solver_Result.Satisfiable assignment
         else Solver_Result.Unsatisfiable)
   ;;
 end
 
 let () =
-  let dimacs_string = In_channel.read_all "../test/test003.cnf" in
+  let dimacs_string = In_channel.input_all In_channel.stdin in
   let fml = Util.parse_dimacs dimacs_string in
-  print_s [%sexp (fml : CNF_Formula.t)];
+  print_s [%sexp (fml : Cnf_formula.t)];
   let assignment = [] in
   let result = DPLL.solve fml assignment in
   print_s [%sexp (result : Solver_Result.t)]
